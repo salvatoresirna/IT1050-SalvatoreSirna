307e5e8cb1ccf6ceb3a5cbe4dffd6dcf44e7b263
salvatoresirna <31554201+salvatoresirna@users.noreply.github.com>
2017-09-09 00:01:13 -0400

    Lab01

---
+++ Lab01/Program.cs
@@ -0,0 +1,19 @@
+﻿// A Hello World! program in C#
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Salvatore Sirna!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
