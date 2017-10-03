edfd0b37d7dc2c00edab99ffb9b825a80acb3392
salvatoresirna <31554201+salvatoresirna@users.noreply.github.com>
2017-10-02 22:29:40 -0400

    Lab05
    Lab05 with Word Document

---
+++ Lab05-SalvatoreSirna/Lab05/Lab05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+   
+        public string bark;
+        public static void bark();
+        {
+        Console.WriteLine( "{0} is Barking. .." , Name);
+        }
+ } 
+public string doTrick;
+public static void doTrick(trickName);
+{
+Console.WriteLine( "{0} is so smart! {0} is doing a(n) {1}" , name, trickName);
+}
---
+++ Lab05-SalvatoreSirna/Lab05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    public string bark;
+    public static void bark();
+ 
+        {
+            Console.WriteLine("{0} is Barking...", name);
+
+        } //End Main
+} // end name space
+   
