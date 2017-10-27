5ebf1056499a10d8519ac94f43e20595b58af714
James Leasure <james.leasure@tri-c.edu>
2017-10-27 09:45:58 -0400

    Grading Comments

--- Lab06-SalvatoreSirna/Lab06-GroceryList/Program.cs
+++ Lab06-SalvatoreSirna/Lab06-GroceryList/Program.cs
@@ -8,7 +8,31 @@ namespace Lab06_GroceryList
 {
     class Grocery
     {
-      
+        static void Main()
+        {
+            string groceryItem = "";
+            double groceryCost = 0d;
+            double totalCost = 0d;
+            string output = "";
+
+            while (true)
+            {
+                Console.Write("Enter grocery item (E to end): ");
+                groceryItem = Console.ReadLine();
+                if (groceryItem == "E")
+                {
+                    break;
+                }
+                Console.Write("Enter grocery cost (numbers only): ");
+                groceryCost = Convert.ToDouble(Console.ReadLine());
+
+                totalCost += groceryCost;
+                output = output + groceryItem + "\t$" + string.Format("{0:0.00}", groceryCost) + "\n";
+            }
+            Console.WriteLine(output);
+            Console.WriteLine("Total Cost $" + string.Format("{0:0.00}", totalCost));
+        }
+    }
         }
     }
 }
